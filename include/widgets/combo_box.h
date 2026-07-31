f864b6977c2458965f3e8f91789d606dc0a0d4e3
stardust <stardust3457@gmail.com>
2026-07-31 19:38:33 +0930

    do not use __interface, use suinterface so it can be a struct on none visual studio compilers

--- include/widgets/combo_box.h
+++ include/widgets/combo_box.h
@@ -4,14 +4,14 @@
 
 static const char * IID_ICOMBOBOX = "2981D907-6EA1-43B4-B4FC-DBC23980A15D";
 
-__interface IComboBox
+suinterface IComboBox
 {
-    int32_t AddItem(const char * item, const char * value) = 0;
-    void ClearContents() = 0;
-    uint32_t GetCount() const = 0;
-    int32_t CurrentIndex() const = 0;
-    SCITER_ELEMENT GetSelectedItem() const = 0;
-    bool SelectItem(int32_t index) = 0;
+    virtual int32_t AddItem(const char * item, const char * value) = 0;
+    virtual void ClearContents() = 0;
+    virtual uint32_t GetCount() const = 0;
+    virtual int32_t CurrentIndex() const = 0;
+    virtual SCITER_ELEMENT GetSelectedItem() const = 0;
+    virtual bool SelectItem(int32_t index) = 0;
 };
 
 bool Register_WidgetComboBox(ISciterUI & SciterUI);
--- src/sciterui/module_resource.h
+++ src/sciterui/module_resource.h
@@ -1,11 +1,12 @@
 #pragma once
+#include <sciter_ui.h>
 #include <memory>
 
 namespace SciterUI
 {
-__interface IModuleResource
+suinterface IModuleResource
 {
-    bool LoadResource(const wchar_t * name, const wchar_t * type, std::unique_ptr<uint8_t> & data, uint32_t & size);
+    virtual bool LoadResource(const wchar_t * name, const wchar_t * type, std::unique_ptr<uint8_t> & data, uint32_t & size) = 0;
 };
 
 class ModuleResource : public IModuleResource
