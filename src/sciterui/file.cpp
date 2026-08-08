82f16ba8f186a1aaa0cf02c6b422c64fbdfce53c
Stardust <36493737+stardust3457@users.noreply.github.com>
2026-08-08 19:54:06 +0930

    Make file class platform agnostic (#22)

--- src/sciterui/file.cpp
+++ src/sciterui/file.cpp
@@ -1,5 +1,27 @@
 #include "file.h"
+#include <cstring>
+
+#ifdef _WIN32
 #include <Windows.h>
+#else
+#include <cstdint>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define INVALID_HANDLE_VALUE ((void *)(intptr_t)-1)
+
+static int HandleToFd(void * h)
+{
+    return (int)(intptr_t)h;
+}
+
+static void * FdToHandle(int fd)
+{
+    return (void *)(intptr_t)fd;
+}
+#endif
 
 namespace SciterUI
 {
@@ -11,10 +33,7 @@ File::File() :
 
 File::~File()
 {
-    if (m_file != INVALID_HANDLE_VALUE)
-    {
-        Close();
-    }
+    Close();
 }
 
 bool File::Open(const char * fileName, uint32_t openFlags)
@@ -29,8 +48,7 @@ bool File::Open(const char * fileName, uint32_t openFlags)
         return false;
     }
 
-    m_file = INVALID_HANDLE_VALUE;
-
+#ifdef _WIN32
     ULONG dwAccess = 0;
     switch (openFlags & 3)
     {
@@ -61,13 +79,11 @@ bool File::Open(const char * fileName, uint32_t openFlags)
         shareMode = 0;
     }
 
-    // map modeNoInherit flag
-    SECURITY_ATTRIBUTES sa;
+    SECURITY_ATTRIBUTES sa = {};
     sa.nLength = sizeof(sa);
     sa.lpSecurityDescriptor = nullptr;
     sa.bInheritHandle = (openFlags & modeNoInherit) == 0;
 
-    // map creation flags
     ULONG createFlag = OPEN_EXISTING;
     if (openFlags & modeCreate)
     {
@@ -80,39 +96,124 @@ bool File::Open(const char * fileName, uint32_t openFlags)
         return false;
     }
     m_file = hFile;
+#else
+    int posixFlags = 0;
+    switch (openFlags & 3)
+    {
+    case modeRead:
+        posixFlags = O_RDONLY;
+        break;
+    case modeWrite:
+        posixFlags = O_WRONLY;
+        break;
+    case modeReadWrite:
+        posixFlags = O_RDWR;
+        break;
+    default:
+        return false;
+    }
+
+    if (openFlags & modeCreate)
+    {
+        posixFlags |= O_CREAT;
+        if ((openFlags & modeNoTruncate) == 0)
+        {
+            posixFlags |= O_TRUNC;
+        }
+    }
+    if (openFlags & modeNoInherit)
+    {
+        posixFlags |= O_CLOEXEC;
+    }
+
+    int fd = ::open(fileName, posixFlags, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (fd < 0)
+    {
+        return false;
+    }
+
+    // Best-effort advisory locking; not a full Windows share-mode equivalent.
+    if ((openFlags & shareExclusive) == shareExclusive || (openFlags & shareDenyWrite) == shareDenyWrite)
+    {
+        struct flock fl = {};
+        fl.l_type = ((openFlags & shareExclusive) == shareExclusive) ? F_WRLCK : F_RDLCK;
+        fl.l_whence = SEEK_SET;
+        if (::fcntl(fd, F_SETLK, &fl) == -1)
+        {
+            ::close(fd);
+            return false;
+        }
+    }
+
+    m_file = FdToHandle(fd);
+#endif
     return true;
 }
 
 bool File::Close()
 {
-    bool bError = true;
-    if (m_file != INVALID_HANDLE_VALUE)
+    if (m_file == INVALID_HANDLE_VALUE)
     {
-        bError = !::CloseHandle(m_file);
+        return true;
     }
+
+    bool success = false;
+#ifdef _WIN32
+    success = ::CloseHandle(m_file) != 0;
+#else
+    success = ::close(HandleToFd(m_file)) == 0;
+#endif
     m_file = INVALID_HANDLE_VALUE;
-    return bError;
+    return success;
 }
 
-uint32_t File::GetLength() const
+uint64_t File::GetLength() const
 {
+    if (m_file == INVALID_HANDLE_VALUE)
+    {
+        return (uint64_t)-1;
+    }
+
+#ifdef _WIN32
     DWORD hiWord = 0;
-    return GetFileSize(m_file, &hiWord);
+    DWORD loWord = ::GetFileSize(m_file, &hiWord);
+    if (loWord == INVALID_FILE_SIZE && ::GetLastError() != NO_ERROR)
+    {
+        return (uint64_t)-1;
+    }
+    return ((uint64_t)hiWord << 32) | (uint64_t)loWord;
+#else
+    struct stat st;
+    if (::fstat(HandleToFd(m_file), &st) != 0)
+    {
+        return (uint64_t)-1;
+    }
+    return (uint64_t)st.st_size;
+#endif
 }
 
-uint32_t File::Read(void * lpBuf, uint32_t nCount)
+uint32_t File::Read(void * buffer, uint32_t bufferSize)
 {
-    if (nCount == 0)
+    if (bufferSize == 0 || m_file == INVALID_HANDLE_VALUE)
     {
         return 0;
     }
 
+#ifdef _WIN32
     DWORD read = 0;
-    if (!::ReadFile(m_file, lpBuf, nCount, &read, nullptr))
+    if (!::ReadFile(m_file, buffer, bufferSize, &read, nullptr))
     {
         return 0;
     }
     return (uint32_t)read;
+#else
+    ssize_t result = ::read(HandleToFd(m_file), buffer, bufferSize);
+    if (result < 0)
+    {
+        return 0;
+    }
+    return (uint32_t)result;
+#endif
 }
 
 bool File::Write(const void * buffer, uint32_t bufferSize)
@@ -121,18 +222,22 @@ bool File::Write(const void * buffer, uint32_t bufferSize)
     {
         return true;
     }
+    if (m_file == INVALID_HANDLE_VALUE)
+    {
+        return false;
+    }
 
-    ULONG written = 0;
+#ifdef _WIN32
+    DWORD written = 0;
     if (!::WriteFile(m_file, buffer, bufferSize, &written, nullptr))
     {
         return false;
     }
-
-    if (written != bufferSize)
-    {
-        return false;
-    }
-    return true;
+    return written == bufferSize;
+#else
+    ssize_t written = ::write(HandleToFd(m_file), buffer, bufferSize);
+    return written >= 0 && (uint32_t)written == bufferSize;
+#endif
 }
 
 } // namespace SciterUI
