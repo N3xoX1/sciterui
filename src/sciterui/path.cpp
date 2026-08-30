9fd8fa41ce5ca34a459a48f6c24ee67df4484833
Stardust <36493737+stardust3457@users.noreply.github.com>
2026-08-30 10:30:49 +0930

    dynamic grow path in Path::SetToModuleDirectory for non win32 (#35)

--- src/sciterui/path.cpp
+++ src/sciterui/path.cpp
@@ -491,17 +491,28 @@ void Path::SetToModuleDirectory()
         buffPath.resize(bufferSize);
     }
 #else
-    char buf[PATH_MAX];
-    ssize_t len = readlink("/proc/self/exe", buf, sizeof(buf) - 1);
-    if (len != -1)
+    size_t bufferSize = 4096;
+    std::vector<char> buffPath(bufferSize);
+
+    while (true)
     {
-        buf[len] = '\0';
-        m_path = buf;
-        SetNameExtension("");
-    }
-    else
-    {
-        m_path = "";
+        ssize_t result = readlink("/proc/self/exe", buffPath.data(), bufferSize);
+
+        if (result < 0)
+        {
+            m_path = "";
+            return;
+        }
+
+        if ((size_t)result < bufferSize)
+        {
+            m_path.assign(buffPath.data(), (size_t)result);
+            SetNameExtension("");
+            return;
+        }
+
+        bufferSize *= 2;
+        buffPath.resize(bufferSize);
     }
 #endif 
 }
